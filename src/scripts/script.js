ef587fcb7188103f89335560ad6a17a84c502d3f
wymuxdev <wymuxgh@gmail.com>
2024-10-24 17:07:15 -0300

    feat: add html layout and basic js structure

---
+++ src/scripts/script.js
@@ -0,0 +1,62 @@
+const inputTask = document.querySelector('#inputTask')
+const addTaskBtn = document.querySelector('#addTaskBtn')
+const elementsList = document.querySelector('#elementsList')
+
+let arrayTasksList = JSON.parse(localStorage.getItem('@tasksList')) || []
+
+renderTasks()
+
+function deleteTask(index) {
+    arrayTasksList.splice(index, 1)
+    renderTasks()
+    saveLocalStorage()
+}
+
+function renderTasks() {
+    elementsList.innerHTML = ''
+
+    arrayTasksList.forEach((task, index) => {
+        const li = createTaskElement(task, index)
+        elementsList.appendChild(li)
+    });
+}
+
+function createTaskElement(task, index) {
+    const li = document.createElement('li')
+    li.textContent = task 
+
+    const btnDelete = createDeleteButton(index)
+    li.appendChild(btnDelete)
+
+    return li
+}
+
+function createDeleteButton(index) {
+    const button = document.createElement('button')
+    button.textContent = 'Excluir'
+
+    button.addEventListener('click', () => deleteTask(index))
+    
+    return button
+}
+
+function saveLocalStorage() {
+    localStorage.setItem('@tasksList', JSON.stringify(arrayTasksList))
+}
+
+function isInputValid() {
+    return inputTask.value.trim() !== ''
+}
+
+addTaskBtn.addEventListener('click', () => {
+    if(!isInputValid()) {
+        alert("Insira algo")
+        return false
+    } 
+
+    const newTask = inputTask.value.trim()
+    arrayTasksList.push(newTask)
+    inputTask.value = ''
+    renderTasks()
+    saveLocalStorage()
+})
